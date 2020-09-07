f05c1f2a8fd38feb6a10d653cf9637320864c06a
hy-chen <47866975+hy-chen@users.noreply.github.com>
2020-09-07 18:57:33 -0400

    Add NYTimes data acquisition scripts

---
+++ nytimes.py
@@ -0,0 +1,54 @@
+import requests
+import json
+import csv
+
+def GET():
+    a = requests.get('https://api.nytimes.com/svc/archive/v1/2020/3.json?api-key=37xNSjQNTdhK18OgxAGjn9WN9QnO1Sn7')
+
+    with open('data.json', 'w') as f:
+        json.dump(a.json(), f)
+
+
+def PARSE():
+    # first, inspect with the Tree View Mode in https://countwordsfree.com/jsonviewer . Then pick the elements we want.
+    with open('data.json') as f:
+        bulk = json.load(f)
+    meta_counts = bulk['response']['meta']['hits']
+    csvfile = open('march_%d.csv'%meta_counts, 'w', newline='', encoding="utf-8")
+    csvwriter = csv.writer(csvfile)
+    # most_need_titles = ['abstract', 'lead_paragraph', 'source', 'headline','keywords','keyword_type','pub_date', 'document_type',
+    # 'news_desk', 'section_name', 'subsection_name', 'type_of_material', '_id']
+    csvwriter.writerow(['abstract', 'web_url', 'lead_paragraph', 'source', 'headline','keywords', 'keyword_type',
+                        'pub_date', 'document_type', 'news_desk', 'section_name', 'subsection_name', 'type_of_material', '_id'])
+    for article in bulk['response']['docs']:
+        row = []
+        for i in ['abstract', 'web_url', 'lead_paragraph', 'source', 'headline','keywords', 'pub_date',
+                  'document_type', 'news_desk', 'section_name', 'subsection_name', 'type_of_material', '_id']:
+            if i == 'headline':
+                try:
+                    a = article[i]['main']
+                except:
+                    a = ''
+            elif i == 'keywords':
+                try:
+                    keywords = article[i]
+                    a = []
+                    b = []
+                    for node in article[i]:
+                        b.append(node['value'])
+                        a.append(node['name'])
+                    row.append(str(b))
+                except:
+                    a = ''
+            else:
+                try:
+                    a = article[i]
+                except:
+                    a = ''
+
+            row.append(str(a))
+        csvwriter.writerow(row)
+
+if __name__ == '__main__':
+    # GET()
+    PARSE()
